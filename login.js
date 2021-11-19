bff55eb520ab3cdc58f1e029f537659390bef32a
Evelyn-Adams <94699970+Evelyn-Adams@users.noreply.github.com>
2021-11-19 22:38:06 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+function login(){
+    player2_name=document.getElementById("player_input_1").value;
+    player1_name=document.getElementById("player_input_2").value;
+
+localStorage.setItem("player1_name",player1_name);
+localStorage.setItem("player2_name",player2_name);
+
+window.location="game.html";
+}
